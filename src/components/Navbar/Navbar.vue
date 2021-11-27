6a4cc0cceafc0ad9cf372675ab657490f61a240d
kyle g <kyleguerrero85@gmail.com>
2021-11-27 13:59:36 -0600

    merge: merged remote into local

--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app-bar app color="primary" dark>
-    <div class="d-flex align-center" to="/">
+    <div class="d-flex align-center">
       <v-img
         alt="Vuetify Logo"
         class="shrink mr-2"
